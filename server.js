d6ff02cae5e22de9998864d3b03ca5baf86fd36a
code-thomasl <thomas.lefebvre01@etu.umontpellier.fr>
2019-12-08 17:43:08 +0100

    Version 1.1.0

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(db)
         app.use(express.static('client/build'));
         app.get('*', (req, res) => {
             res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-        })
+        });
     }
 
     const port = process.env.PORT || 5000;
